bce3a340839da36b3c9ffe90e79ea455ca9afa98
mrkirby153 <mr.austinwhyte@gmail.com>
2016-05-29 10:03:01 -0700

    Make sure the player and the role exists before assigning them

--- src/main/java/me/mrkirby153/uhc/bot/network/commands/AssignRole.java
+++ src/main/java/me/mrkirby153/uhc/bot/network/commands/AssignRole.java
@@ -5,16 +5,19 @@ import com.google.common.io.ByteArrayDataOutput;
 import me.mrkirby153.uhc.bot.Main;
 import me.mrkirby153.uhc.bot.discord.ServerHandler;
 import me.mrkirby153.uhc.bot.network.NetworkCommand;
+import net.dv8tion.jda.entities.User;
 
 import java.util.UUID;
 
-public class AssignRole implements NetworkCommand{
+public class AssignRole implements NetworkCommand {
 
     @Override
     public void process(ServerHandler.DiscordServer server, ByteArrayDataInput input, ByteArrayDataOutput response) {
         UUID u = UUID.fromString(input.readUTF());
         String role = input.readUTF();
         ServerHandler.DiscordRank rank = server.getRankByName(role);
-        rank.assign(Main.discordHandler.getUser(u));
+        User user = Main.discordHandler.getUser(u);
+        if (user != null && rank != null)
+            rank.assign(user);
     }
 }
